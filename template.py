7ff369c56dfb6c44c37ddf86e0c985278d289340
rohit <rs5294645@gmail.com>
2025-05-08 01:13:06 +0530

    folder structure corrected

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "animalintrusion"
+project_name = "animal_intrusion"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
