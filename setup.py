c09b95642e0464c460046cfea7f7418bd81aa1dd
rohit <rs5294645@gmail.com>
2025-05-08 03:12:49 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'animal intrusion',
+    version= '0.0.0',
+    author= 'Rohit sharma',
+    author_email= 'rs5294645@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
